dd7246f818662f0dc55ff27488a06bd77b707071
rob-moore1894 <moore.re@gmail.com>
2019-02-25 13:21:08 -0600

    Completed Contoller Exercises

---
+++ src/main/java/com/codeup/codeupspring/controllers/PostController.java
@@ -0,0 +1,34 @@
+package com.codeup.codeupspring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String posts() {
+        return "Posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String indivdualPosts(){
+        return "View an individual post";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createPostForm(){
+        return "View the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost(){
+        return "Create a new post";
+    }
+
+}
